f23ecd3ad4a09d3480aa10e1ddb8ec7a32eab197
Lucas Meine <lucas.meine@citara.io>
2018-10-10 11:02:47 -0300

    Devmind commit in 2018-10-10 11:02:46

--- app/config/privateResources.php
+++ app/config/privateResources.php
@@ -3,25 +3,4 @@
 use Phalcon\Config;
 use Phalcon\Logger;
 
-return new Config([
-    'privateResources' => [
-        'users' => [
-            'index',
-            'search',
-            'edit',
-            'create',
-            'delete',
-            'changePassword'
-        ],
-        'profiles' => [
-            'index',
-            'search',
-            'edit',
-            'create',
-            'delete'
-        ],
-        'permissions' => [
-            'index'
-        ]
-    ]
-]);
+return new Config();
