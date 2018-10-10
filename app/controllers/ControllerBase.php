11651729c05d07356ce53cc34add38f90de749ba
Lucas Meine <lucas.meine@citara.io>
2018-10-10 11:05:23 -0300

    Devmind commit in 2018-10-10 11:05:23

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -22,45 +22,6 @@ class ControllerBase extends Controller
     public function beforeExecuteRoute(Dispatcher $dispatcher)
     {
         $controllerName = $dispatcher->getControllerName();
-
-        // Only check permissions on private controllers
-        if ($this->acl->isPrivate($controllerName)) {
-
-            // Get the current identity
-            $identity = $this->auth->getIdentity();
-
-            // If there is no identity available the user is redirected to index/index
-            if (!is_array($identity)) {
-
-                $this->flash->notice('You don\'t have access to this module: private');
-
-                $dispatcher->forward([
-                    'controller' => 'index',
-                    'action' => 'index'
-                ]);
-                return false;
-            }
-
-            // Check if the user have permission to the current option
-            $actionName = $dispatcher->getActionName();
-            if (!$this->acl->isAllowed($identity['profile'], $controllerName, $actionName)) {
-
-                $this->flash->notice('You don\'t have access to this module: ' . $controllerName . ':' . $actionName);
-
-                if ($this->acl->isAllowed($identity['profile'], $controllerName, 'index')) {
-                    $dispatcher->forward([
-                        'controller' => $controllerName,
-                        'action' => 'index'
-                    ]);
-                } else {
-                    $dispatcher->forward([
-                        'controller' => 'user_control',
-                        'action' => 'index'
-                    ]);
-                }
-
-                return false;
-            }
-        }
+        return true;
     }
 }
