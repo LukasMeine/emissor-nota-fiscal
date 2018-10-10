75a05981649bd40491aba9f302825dcd6ab0dfd5
Lucas Meine <lucas.meine@citara.io>
2018-10-10 11:02:06 -0300

    Devmind commit in 2018-10-10 11:02:05

--- app/config/services.php
+++ app/config/services.php
@@ -67,18 +67,6 @@ $di->set('view', function () {
     return $view;
 }, true);
 
-/**
- * Database connection is created based in the parameters defined in the configuration file
- */
-$di->set('db', function () {
-    $config = $this->getConfig();
-    return new DbAdapter([
-        'host' => $config->database->host,
-        'username' => $config->database->username,
-        'password' => $config->database->password,
-        'dbname' => $config->database->dbname
-    ]);
-});
 
 /**
  * If the configuration specify the use of metadata adapter use it or use memory otherwise
