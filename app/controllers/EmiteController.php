f769777eb4e562d7415e1643a7e6ea20c90c1ccd
Igor campos <igorocampos@gmail.com>
2019-10-01 12:20:45 -0300

    Function que converte codigo IBGE da UF em Sigla

--- app/controllers/EmiteController.php
+++ app/controllers/EmiteController.php
@@ -52,6 +52,45 @@ class EmiteController extends ControllerBase
         }
     }
 
+    private function ibge_sigla($ibge)
+    {
+        switch ($ibge) {
+            case 11: return "RO";
+            case 12: return "AC";
+            case 13: return "AM";
+            case 14: return "RR";
+            case 15: return "PA";
+            case 16: return "AP";
+            case 17: return "TO";
+            
+            case 21: return "MA";
+            case 22: return "PI";
+            case 23: return "CE";
+            case 24: return "RN";
+            case 25: return "PB";
+            case 26: return "PE";
+            case 27: return "AL";
+            case 28: return "SE";
+            case 29: return "BA";
+
+            case 31: return "MG";
+            case 32: return "ES";
+            case 33: return "RJ";
+            case 35: return "SP";
+
+            case 41: return "PR";
+            case 42: return "SC";
+            case 43: return "RS";
+
+            case 50: return "MS";
+            case 51: return "MT";
+            case 52: return "GO";
+            case 53: return "DF";
+
+            default: throw new Exception('IBGE da UF inválido.');
+        }
+    }
+
 
     private function get_xml()
     {
